c7a39499d111fe2e4ca002fd167b90241bea8718
anisa6121 <you@example.com>
2022-11-10 10:46:50 +0600

    single-data-return

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // dotenv
 require("dotenv").config();
@@ -27,55 +27,52 @@ const client = new MongoClient(uri, {
 });
 async function run() {
 	try {
-	
-// database Name & collection Name
- const serviceCollection = client.db("foodService").collection("foodServices");
-  
-        
-// get all data
-        app.get("/allServices", async(req, res) => {
-    const query = {};
-    const cursor = serviceCollection.find(query);
-    const services = await cursor.toArray();
-    res.send(services);
-	
-        });
-        
-        // get three data
-	  app.get("/someServices", async (req, res) => {
+		// database Name & collection Name
+		const serviceCollection = client
+			.db("foodService")
+			.collection("foodServices");
+
+		// get all data
+		app.get("/allServices", async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.toArray();
+			res.send(services);
+		});
+
+		// Single Service get
+
+		app.get("/allServices/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id)};
+
+	const singleService = await serviceCollection.findOne(query);
+
+			res.send(singleService);
+		});
+
+		// get three data
+		app.get("/someServices", async (req, res) => {
 			const query = {};
 			const cursor = serviceCollection.find(query);
 			const services = await cursor.limit(3).toArray();
 			res.send(services);
 		});
 
-		
-// Add Service
- app.post("/allServices", async(req, res) => {
-  
-    console.log('post Api called')
-    const addService = req.body
-    console.log(addService);
+		// Add Service
+		app.post("/allServices", async (req, res) => {
+			console.log("post Api called");
+			const addService = req.body;
+			console.log(addService);
 
-    const result = await serviceCollection.insertOne(addService);
-	console.log(result);
+			const result = await serviceCollection.insertOne(
+				addService
+			);
+			console.log(result);
 
-	res.send(result);
-             
- });
-		
-
-		
-
-			
-		
-
-
-	
-
-		
-		
-    }
+			res.send(result);
+		});
+	}
     finally {
 	}
 }
